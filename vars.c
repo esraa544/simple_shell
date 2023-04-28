3790cd4989d542102f0dfdca1af05953006ec854
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-04-28 23:24:16 +0200

    task

--- vars.c
+++ vars.c
@@ -2,9 +2,6 @@
 
 /**
  * is_chain - test if current char in buffer is a chain delimeter
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
  *
  * Return: 1 if chain delimeter, 0 otherwise
  */
@@ -30,21 +27,13 @@ int is_chain(info_t *info, char *buf, size_t *p)
 		info->cmd_buf_type = CMD_CHAIN;
 	}
 	else
+	{
 		return (0);
+	}
 	*p = j;
 	return (1);
 }
 
-/**
- * check_chain - checks we should continue chaining based on last status
- * @info: the parameter struct
- * @buf: the char buffer
- * @p: address of current position in buf
- * @i: starting position in buf
- * @len: length of buf
- *
- * Return: Void
- */
 void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 {
 	size_t j = *p;
@@ -69,12 +58,6 @@ void check_chain(info_t *info, char *buf, size_t *p, size_t i, size_t len)
 	*p = j;
 }
 
-/**
- * replace_alias - replaces an aliases in the tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, 0 otherwise
- */
 int replace_alias(info_t *info)
 {
 	int i;
@@ -85,25 +68,25 @@ int replace_alias(info_t *info)
 	{
 		node = node_starts_with(info->alias, info->argv[0], '=');
 		if (!node)
+		{
 			return (0);
+		}
 		free(info->argv[0]);
 		p = _strchr(node->str, '=');
 		if (!p)
+		{
 			return (0);
+		}
 		p = _strdup(p + 1);
 		if (!p)
+		{
 			return (0);
+		}
 		info->argv[0] = p;
 	}
 	return (1);
 }
 
-/**
- * replace_vars - replaces vars in the tokenized string
- * @info: the parameter struct
- *
- * Return: 1 if replaced, 0 otherwise
- */
 int replace_vars(info_t *info)
 {
 	int i = 0;
@@ -112,7 +95,9 @@ int replace_vars(info_t *info)
 	for (i = 0; info->argv[i]; i++)
 	{
 		if (info->argv[i][0] != '$' || !info->argv[i][1])
+		{
 			continue;
+		}
 
 		if (!_strcmp(info->argv[i], "$?"))
 		{
@@ -139,13 +124,6 @@ int replace_vars(info_t *info)
 	return (0);
 }
 
-/**
- * replace_string - replaces string
- * @old: address of old string
- * @new: new string
- *
- * Return: 1 if replaced, 0 otherwise
- */
 int replace_string(char **old, char *new)
 {
 	free(*old);
