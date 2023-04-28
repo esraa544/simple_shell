42dec625d325d3fdbf64ff5fb4fa437a2405d083
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-04-28 23:26:12 +0200

    task

--- builtin.c
+++ builtin.c
@@ -2,10 +2,8 @@
 
 /**
  * _myexit - exits the shell
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
- *  Return: exits with a given exit status
- *         (0) if info.argv[0] != "exit"
+ *
+ *  Return: exits with a given exit status (0) if info.argv[0] != "exit"
  */
 int _myexit(info_t *info)
 {
