2441517c23309b76a982048ac1d1ba29df60a05c
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-04-28 22:34:46 +0200

    task

--- errors1.c
+++ errors1.c
@@ -12,7 +12,9 @@ int _erratoi(char *s)
 	unsigned long int result = 0;
 
 	if (*s == '+')
-		s++;  /* TODO: why does this make main return 255? */
+	{
+		s++;
+	}
 	for (i = 0;  s[i] != '\0'; i++)
 	{
 		if (s[i] >= '0' && s[i] <= '9')
@@ -20,21 +22,18 @@ int _erratoi(char *s)
 			result *= 10;
 			result += (s[i] - '0');
 			if (result > INT_MAX)
+			{
 				return (-1);
+			}
 		}
 		else
+		{
 			return (-1);
+		}
 	}
 	return (result);
 }
 
-/**
- * print_error - prints an error message
- * @info: the parameter & return info struct
- * @estr: string containing specified error type
- * Return: 0 if no numbers in string, converted number otherwise
- *        -1 on error
- */
 void print_error(info_t *info, char *estr)
 {
 	_eputs(info->fname);
@@ -46,13 +45,6 @@ void print_error(info_t *info, char *estr)
 	_eputs(estr);
 }
 
-/**
- * print_d - function prints a decimal (integer) number (base 10)
- * @input: the input
- * @fd: the filedescriptor to write to
- *
- * Return: number of characters printed
- */
 int print_d(int input, int fd)
 {
 	int (*__putchar)(char) = _putchar;
@@ -60,7 +52,9 @@ int print_d(int input, int fd)
 	unsigned int _abs_, current;
 
 	if (fd == STDERR_FILENO)
+	{
 		__putchar = _eputchar;
+	}
 	if (input < 0)
 	{
 		_abs_ = -input;
@@ -68,7 +62,9 @@ int print_d(int input, int fd)
 		count++;
 	}
 	else
+	{
 		_abs_ = input;
+	}
 	current = _abs_;
 	for (i = 1000000000; i > 1; i /= 10)
 	{
@@ -85,14 +81,6 @@ int print_d(int input, int fd)
 	return (count);
 }
 
-/**
- * convert_number - converter function, a clone of itoa
- * @num: number
- * @base: base
- * @flags: argument flags
- *
- * Return: string
- */
 char *convert_number(long int num, int base, int flags)
 {
 	static char *array;
@@ -117,24 +105,22 @@ char *convert_number(long int num, int base, int flags)
 	} while (n != 0);
 
 	if (sign)
+	{
 		*--ptr = sign;
+	}
 	return (ptr);
 }
 
-/**
- * remove_comments - function replaces first instance of '#' with '\0'
- * @buf: address of the string to modify
- *
- * Return: Always 0;
- */
 void remove_comments(char *buf)
 {
 	int i;
 
 	for (i = 0; buf[i] != '\0'; i++)
+	{
 		if (buf[i] == '#' && (!i || buf[i - 1] == ' '))
 		{
 			buf[i] = '\0';
 			break;
 		}
+	}
 }
