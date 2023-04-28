b4cf4875477c1d781f99aaa31e7c3f65fcfaed42
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-04-28 23:20:17 +0200

    task

--- string1.c
+++ string1.c
@@ -2,8 +2,6 @@
 
 /**
  * _strcpy - copies a string
- * @dest: the destination
- * @src: the source
  *
  * Return: pointer to destination
  */
@@ -12,7 +10,9 @@ char *_strcpy(char *dest, char *src)
 	int i = 0;
 
 	if (dest == src || src == 0)
+	{
 		return (dest);
+	}
 	while (src[i])
 	{
 		dest[i] = src[i];
@@ -22,41 +22,39 @@ char *_strcpy(char *dest, char *src)
 	return (dest);
 }
 
-/**
- * _strdup - duplicates a string
- * @str: the string to duplicate
- *
- * Return: pointer to the duplicated string
- */
 char *_strdup(const char *str)
 {
 	int length = 0;
 	char *ret;
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 	while (*str++)
+	{
 		length++;
+	}
 	ret = malloc(sizeof(char) * (length + 1));
 	if (!ret)
+	{
 		return (NULL);
+	}
 	for (length++; length--;)
+	{
 		ret[length] = *--str;
+	}
 	return (ret);
 }
 
-/**
- *_puts - prints an input string
- *@str: the string to be printed
- *
- * Return: Nothing
- */
 void _puts(char *str)
 {
 	int i = 0;
 
 	if (!str)
+	{
 		return;
+	}
 	while (str[i] != '\0')
 	{
 		_putchar(str[i]);
@@ -64,13 +62,6 @@ void _puts(char *str)
 	}
 }
 
-/**
- * _putchar - writes the character c to stdout
- * @c: The character to print
- *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
- */
 int _putchar(char c)
 {
 	static int i;
@@ -82,6 +73,8 @@ int _putchar(char c)
 		i = 0;
 	}
 	if (c != BUF_FLUSH)
+	{
 		buf[i++] = c;
+	}
 	return (1);
 }
