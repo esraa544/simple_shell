c4c76876011f82137623ba6df75f23e78c3ab92f
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-04-28 22:40:08 +0200

    Update getenv.c

--- getenv.c
+++ getenv.c
@@ -2,8 +2,6 @@
 
 /**
  * get_environ - returns the string array copy of our environ
- * @info: Structure containing potential arguments. Used to maintain
- *          constant function prototype.
  * Return: Always 0
  */
 char **get_environ(info_t *info)
@@ -17,13 +15,6 @@ char **get_environ(info_t *info)
 	return (info->environ);
 }
 
-/**
- * _unsetenv - Remove an environment variable
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- *  Return: 1 on delete, 0 otherwise
- * @var: the string env var property
- */
 int _unsetenv(info_t *info, char *var)
 {
 	list_t *node = info->env;
@@ -49,15 +40,6 @@ int _unsetenv(info_t *info, char *var)
 	return (info->env_changed);
 }
 
-/**
- * _setenv - Initialize a new environment variable,
- *             or modify an existing one
- * @info: Structure containing potential arguments. Used to maintain
- *        constant function prototype.
- * @var: the string env var property
- * @value: the string env var value
- *  Return: Always 0
- */
 int _setenv(info_t *info, char *var, char *value)
 {
 	char *buf = NULL;
@@ -65,11 +47,15 @@ int _setenv(info_t *info, char *var, char *value)
 	char *p;
 
 	if (!var || !value)
+	{
 		return (0);
+	}
 
 	buf = malloc(_strlen(var) + _strlen(value) + 2);
 	if (!buf)
+	{
 		return (1);
+	}
 	_strcpy(buf, var);
 	_strcat(buf, "=");
 	_strcat(buf, value);
