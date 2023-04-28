ee0b682658b403d7f6f132d2475aefa4ce1f9a57
Esraa Mohamed <76407362+esraa544@users.noreply.github.com>
2023-04-28 22:41:11 +0200

    task

--- getinfo.c
+++ getinfo.c
@@ -2,7 +2,6 @@
 
 /**
  * clear_info - initializes info_t struct
- * @info: struct address
  */
 void clear_info(info_t *info)
 {
@@ -12,11 +11,6 @@ void clear_info(info_t *info)
 	info->argc = 0;
 }
 
-/**
- * set_info - initializes info_t struct
- * @info: struct address
- * @av: argument vector
- */
 void set_info(info_t *info, char **av)
 {
 	int i = 0;
@@ -44,11 +38,6 @@ void set_info(info_t *info, char **av)
 	}
 }
 
-/**
- * free_info - frees info_t struct fields
- * @info: struct address
- * @all: true if freeing all fields
- */
 void free_info(info_t *info, int all)
 {
 	ffree(info->argv);
@@ -57,18 +46,28 @@ void free_info(info_t *info, int all)
 	if (all)
 	{
 		if (!info->cmd_buf)
+		{
 			free(info->arg);
+		}
 		if (info->env)
+		{
 			free_list(&(info->env));
+		}
 		if (info->history)
+		{
 			free_list(&(info->history));
+		}
 		if (info->alias)
+		{
 			free_list(&(info->alias));
+		}
 		ffree(info->environ);
 			info->environ = NULL;
 		bfree((void **)info->cmd_buf);
 		if (info->readfd > 2)
+		{
 			close(info->readfd);
+		}
 		_putchar(BUF_FLUSH);
 	}
 }
